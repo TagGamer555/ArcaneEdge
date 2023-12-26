275a7095ee7a924067268f8462ef4c84f8898071
Alexander <alexandr.11071997@gmail.com>
2023-12-26 21:25:46 +0000

    oops forgot some stuff

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -1,4 +1,7 @@
 {
 	//EXAMPLE (replace <> with actual information)
 	// const <exampleStructure> = extend(<TYPE>, "<example-structure>", {});
+	
+	//  REGION - TURRETS  //
+	const starStriker = extend(PowerTurret, "star-striker", {});
 }
