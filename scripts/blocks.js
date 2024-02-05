73df2b4954b0ace975437bb08021f16d95617f5e
Alexander <69044271+TagGamer555@users.noreply.github.com>
2024-02-05 11:16:19 +0000

    bridge conveyor. :D

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -2,8 +2,12 @@
 	// EXAMPLE (replace <> with actual information)
 	// const <exampleStructure> = extend(<TYPE>, "<example-structure>", {});
 	
-	//  REGION - TURRETS  //
+	////// ORIGINAL //////
+	
+	// REGION - TURRETS //
 	const starStriker = extend(PowerTurret, "star-striker", {});
+	
+	////// CLASSIC PORT //////
 	
 	// REGION - DRILLS //
 	const stoneDrill = extend(Drill, "stone-drill", {});
@@ -16,11 +20,13 @@
 	const steelConveyor = extend(Conveyor, "steel-conveyor", {});
 	const stoneRouter = extend(Router, "stone-router", {})
 	const stoneJunction = extend(Junction, "stone-junction", {})
+	const ironBridgeConveyor = extend(ItemBridge, "iron-bridge-conveyor", {})
 	
 	// REGION - CRAFTING //
 	const steelSmelter = extend(GenericCrafter, "steel-smelter", {});
 	const diriumCrucible = extend(GenericCrafter, "dirium-crucible", {});
 	const solidifier = extend(GenericCrafter, "solidifier", {});
+	const coalExtractor = extend(GenericCrafter, "coalExtractor", {});
 	const oilSolidifier = extend(GenericCrafter, "oil-solidifier", {});
 	const lavaSmelter = extend(GenericCrafter, "lava-smelter", {});
 	const titaniumExtractor = extend(GenericCrafter, "titanium-extractor", {});
