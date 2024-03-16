b5de33514d518e8e110bdf536dc11219a027b2d1
Alexander <69044271+TagGamer555@users.noreply.github.com>
2024-03-16 19:25:14 +0000

    stuf

--- scripts/items.js
+++ scripts/items.js
@@ -1,3 +1,12 @@
 {
 	const stone = extend(Item, "stone", {});
+	const scalar = extend(Item, "scalar", {});
+	const iron = extend(Item, "iron", {});
+	const steel = extend(Item, "steel", {});
+	const vector = extend(Item, "vector", {});
+	const uranium = extend(Item, "uranium", {});
+	const matrix = extend(Item, "matrix", {});
+	const dirium = extend(Item, "dirium", {});
+	const tensor = extend(Item, "tensor", {});
+	const manifold = extend(Item, "manifold", {});
 }
