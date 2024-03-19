50148b3d5412698a8cc5f0f82e118926904f9659
Alexander <69044271+TagGamer555@users.noreply.github.com>
2024-03-19 19:22:27 +0000

    sand sifter test resprite

--- scripts/items.js
+++ scripts/items.js
@@ -2,14 +2,16 @@
 	const stone = extend(Item, "stone", {});
 	const algae = extend(Item, "algae", {});
 	const sulphur = extend(Item, "sulphur", {});
-	const scalar = extend(Item, "scalar", {});
 	const iron = extend(Item, "iron", {});
 	const steel = extend(Item, "steel", {});
 	const plate = extend(Item, "plate", {});
-	const vector = extend(Item, "vector", {});
 	const uranium = extend(Item, "uranium", {});
-	const matrix = extend(Item, "matrix", {});
 	const dirium = extend(Item, "dirium", {});
+	/*
+	const scalar = extend(Item, "scalar", {});
+	const vector = extend(Item, "vector", {});
+	const matrix = extend(Item, "matrix", {});
 	const tensor = extend(Item, "tensor", {});
 	const manifold = extend(Item, "manifold", {});
+	*/
 }
